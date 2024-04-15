8466979fb56b93063645ee9996268b585ba6e1c2
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2024-04-15 17:02:16 -0600

    Initial title screen
    Added fonts and developed an initial version of title screen

--- GalactaTEC/Assets/Scripts/cnvTitleScript.cs
+++ GalactaTEC/Assets/Scripts/cnvTitleScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class btn1PlayerScript : MonoBehaviour
+public class cnvTitleScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,18 +14,36 @@ public class btn1PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void _1PlayerButtonOnClick()
     {
         // ToDo: Updates quantity of players to 1
+        Debug.Log("Updates quantity of players to 1");
+
         SceneManager.LoadScene("LoginScene");
     }
 
     public void _2PlayersButtonOnClick()
     {
         // ToDo: Updates quantity of players to 2
+        Debug.Log("Updates quantity of players to 2");
+
         SceneManager.LoadScene("LoginScene");
+    }
+
+    public void closeButtonOnClick()
+    {
+        Application.Quit();
+    }
+
+    public void helpButtonOnClick()
+    {
+        // ToDo: Opens help window
+        Debug.Log("Opens help window");
     }
 }
