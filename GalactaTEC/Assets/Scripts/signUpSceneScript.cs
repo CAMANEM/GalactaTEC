6621d1f6579035eb4d36c2b201fee9002bb3d0b0
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2024-04-19 00:04:25 -0600

    Sign up scene checkovers
    Sign up scene already checks username, email and password are available and valid

--- GalactaTEC/Assets/Scripts/signUpSceneScript.cs
+++ GalactaTEC/Assets/Scripts/signUpSceneScript.cs
@@ -2,13 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using TMPro;
 
+[System.Serializable]
+public class User
+{
+    public string name;
+    public string email;
+    public string username;
+    public string password;
+}
+
+[System.Serializable]
+public class Users
+{
+    public List<User> users;
+    public int cuantity;
+}
+
+[System.Serializable]
 public class signUpSceneScript : MonoBehaviour  
 {
+    // GUI elements
+    public RawImage imgUserName;
+    public RawImage imgShipName;
+
+    public TMP_InputField inpName;
+    public TMP_InputField inpEmail;
+    public TMP_InputField inpUsername;
+    public TMP_InputField inpPassword;
+
+    // Variables
     string imagePath;
-    
-    public RawImage userImage;
-    public RawImage shipImage;
+
+    string usersPath = Application.dataPath + "/Data/users.json";
 
 
     // Start is called before the first frame update
@@ -36,11 +68,11 @@ public class signUpSceneScript : MonoBehaviour
                 Texture2D texture = new Texture2D(2, 2);
                 texture.LoadImage(imageBytes);
 
-                userImage.texture = texture;
+                imgUserName.texture = texture;
             }
         } catch
         {
-            Debug.LogError("Something went wrong loading the selected user image. Image selected: " + userImage);
+            Debug.LogError("Something went wrong loading the selected user image. Image selected: " + imgUserName);
         }
     }
 
@@ -57,12 +89,271 @@ public class signUpSceneScript : MonoBehaviour
                 Texture2D texture = new Texture2D(2, 2);
                 texture.LoadImage(imageBytes);
 
-                shipImage.texture = texture;
+                imgShipName.texture = texture;
             }
         }
         catch
         {
-            Debug.LogError("Something went wrong loading the selected ship image. Image selected: " + shipImage);
+            Debug.LogError("Something went wrong loading the selected ship image. Image selected: " + imgShipName);
+        }
+    }
+
+    private List<User> getSignedUsers()
+    {
+        string usersJSON = File.ReadAllText(usersPath);
+
+        Users users = JsonUtility.FromJson<Users>(usersJSON);
+
+        return users.users;
+    }
+
+    public void createAccount()
+    {
+        if (File.Exists(usersPath))
+        {
+            string usersJSON = File.ReadAllText(usersPath);
+
+            Users users = JsonUtility.FromJson<Users>(usersJSON);
+
+            User user = new User();
+            user.name = inpName.text;
+            user.email = inpEmail.text;
+            user.username = inpUsername.text;
+            user.password = inpPassword.text;
+
+            if (emailIsUnique(user.email) && usernameIsUnique(user.username) && passwordIsValid(user.password))
+            {
+                if (emailExists(user.email))
+                {
+                    users.users.Add(user);
+                    users.cuantity += 1;
+
+                    string updatedJSON = JsonUtility.ToJson(users);
+
+                    File.WriteAllText(usersPath, updatedJSON);
+
+                    //MessageBox.Show("What are you waiting for? Let's play!", "Account created succesfully!");
+                    Debug.Log("Account created succesfully!");
+                }
+            }
+        }
+        else
+        {
+            User user = new User();
+            user.name = inpName.text;
+            user.email = inpEmail.text;
+            user.username = inpUsername.text;
+            user.password = inpPassword.text;
+
+            if (passwordIsValid(user.password))
+            {
+                if (emailExists(user.email))
+                {
+                    List<User> userList = new List<User>();
+                    userList.Add(user);
+                    Users users = new Users();
+                    users.users = userList;
+                    users.cuantity = userList.Count;
+
+                    string userJson = JsonUtility.ToJson(users);
+
+                    File.WriteAllText(usersPath, userJson);
+                    //MessageBox.Show("What are you waiting for? Let's play!", "Account created succesfully!");
+                    Debug.Log("Account created succesfully!");
+                }
+            }     
+        }
+    }
+
+    private bool usernameIsUnique(string username)
+    {
+        List<User> users = getSignedUsers();
+
+        if(users.Exists(user => user.username == username) || username == "")
+        {
+            //MessageBox.Show("The given username is already used", "Try another username please.");
+            Debug.Log("The given username is not available");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private bool emailIsUnique(string email)
+    {
+        List<User> users = getSignedUsers();
+
+        if(users.Exists(user => user.email == email) || email == "")
+        {
+            //MessageBox.Show("The given email is already used", "Try another email please.");
+            Debug.Log("The given email is not available");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private bool passwordIsValid(string password)
+    {
+        if (Regex.IsMatch(password, ".{7,}"))
+        {
+            if (Regex.IsMatch(password, "[A-Z]"))
+            {
+                if (Regex.IsMatch(password, "[a-z]"))
+                {
+                    if (Regex.IsMatch(password, "[0-9]"))
+                    {
+                        if (Regex.IsMatch(password, @"[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]"))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            /*MessageBox.Show("The given password does not match the required conditions", "Password conditions:\n\n" +
+                                "- At least 7 characters\n- At least one uppercase character\n- At least one lowercase character\n" +
+                                "- At least one number\n- At least one special character\n\n" +
+                                "The given password does not have enough special characters");*/
+                            Debug.Log("The given password does not have enough special characters");
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        /*MessageBox.Show("The given password does not match the required conditions", "Password conditions:\n\n" +
+                                "- At least 7 characters\n- At least one uppercase character\n- At least one lowercase character\n" +
+                                "- At least one number\n- At least one special character\n\n" +
+                                "The given password does not have enough numbers");*/
+                        Debug.Log("The given password does not have enough numbers");
+                        return false;
+                    }
+                }
+                else
+                {
+                    /*MessageBox.Show("The given password does not match the required conditions", "Password conditions:\n\n" +
+                                "- At least 7 characters\n- At least one uppercase character\n- At least one lowercase character\n" +
+                                "- At least one number\n- At least one special character\n\n" +
+                                "The given password does not have enough lowercase characters");*/
+                    Debug.Log("The given password does not have enough lowercase characters");
+                    return false;
+                }
+            }
+            else
+            {
+                /*MessageBox.Show("The given password does not match the required conditions", "Password conditions:\n\n" +
+                                "- At least 7 characters\n- At least one uppercase character\n- At least one lowercase character\n" +
+                                "- At least one number\n- At least one special character\n\n" +
+                                "The given password does not have enough uppercase characters");*/
+                Debug.Log("The given password does not have enough uppercase characters");
+                return false;
+            }
+        }
+        else
+        {
+            /*MessageBox.Show("The given password does not match the required conditions", "Password conditions:\n\n" +
+                                "- At least 7 characters\n- At least one uppercase character\n- At least one lowercase character\n" +
+                                "- At least one number\n- At least one special character\n\n" +
+                                "The given password does not have enough characters");*/
+            Debug.Log("The given password does not have enough characters");
+            return false;
+        }
+    }
+
+    private bool emailExists(string email)
+    {
+        if (isEmailValid(email))
+        {
+            string[] parts = email.Split('@');
+            string domain = parts[1];
+
+            if (IsDomainValid(domain))
+            {
+                return sendConfirmationEmail(email);
+            }
+            else
+            {
+                /*MessageBox.Show("Wrong email domain", "The given email domain does not exist");*/
+                Debug.Log("The given email domain does not exist");
+                return false;
+            }
+        }
+        else
+        {
+            //MessageBox.Show("Wrong email", "The given email does not exist");
+            Debug.Log("The given email does not exist");
+            return false;
+        }
+    }
+
+    private bool isEmailValid(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return false;
+
+        try
+        {
+            var addr = new MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private bool IsDomainValid(string domain)
+    {
+        try
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(domain);
+            return ipHost.AddressList.Length > 0;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private bool sendConfirmationEmail(string email)
+    {
+        string fromMail = "pipodevs.galactatec@gmail.com";
+        string fromPassword = "frezafraqedljgwj";
+
+        MailMessage message = new MailMessage();
+        message.From = new MailAddress(fromMail);
+        message.Subject = "Welcome to GalactaTEC!";
+        message.To.Add(new MailAddress(email));
+        message.Body = "<html><body>" +
+            "We are very excited to have you here!<br>" +
+            "This help us to confirm this email is yours, now we can be in touch!<br>" +
+            "Don't worry, you are not expected to do anything, just close this mail and start enjoying the game.<br>" +
+            "This is the beggining of something big!<br><br>" +
+            "Welcome,<br>" +
+            "The GalactaTEC team." +
+            "</body></html>";
+        message.IsBodyHtml = true;
+
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential(fromMail, fromPassword),
+            EnableSsl = true,
+        };
+
+        try
+        {
+            smtpClient.Send(message);
+            return true;
+        }
+        catch
+        {
+            //MessageBox.Show("Wrong email", "We have troubles to confirm your email address, please, check your email and try again");
+            Debug.Log("We have troubles to confirm your email address, please, check your email and try again");
+            return false;
         }
     }
 }
+
