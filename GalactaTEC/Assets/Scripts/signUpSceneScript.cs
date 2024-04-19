adbeb94cc5b3cdc4b5fbba5f024022d5c0ba92c0
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2024-04-18 18:31:57 -0600

    Sign Up GUI
    Developed a complete graphic user interface for users sign up

---
+++ GalactaTEC/Assets/Scripts/signUpSceneScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class signUpSceneScript : MonoBehaviour  
+{
+    string imagePath;
+    
+    public RawImage userImage;
+    public RawImage shipImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void selectUserImage()
+    {
+        try
+        {
+            string imagePath = UnityEditor.EditorUtility.OpenFilePanel("Select a new user image", "", "png,jpg,jpeg,gif,bmp");
+
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage(imageBytes);
+
+                userImage.texture = texture;
+            }
+        } catch
+        {
+            Debug.LogError("Esta función solo está disponible en el editor de Unity.");
+        }
+    }
+
+    public void selectShipImage()
+    {
+        try
+        {
+            string imagePath = UnityEditor.EditorUtility.OpenFilePanel("Select a new ship image", "", "png,jpg,jpeg,gif,bmp");
+
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage(imageBytes);
+
+                shipImage.texture = texture;
+            }
+        }
+        catch
+        {
+            Debug.LogError("Esta función solo está disponible en el editor de Unity.");
+        }
+    }
+}
