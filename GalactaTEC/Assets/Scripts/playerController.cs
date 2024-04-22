cf3f8b0999b036908a1448e3ab644aac0d8c4386
Óscar Soto Varela <camanem10@hotmail.com>
2024-04-22 10:35:04 -0700

    Power activation

--- GalactaTEC/Assets/Scripts/playerController.cs
+++ GalactaTEC/Assets/Scripts/playerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
 
     public AudioSource source;
     public AudioClip audioClip;
+    public AudioClip bonusSound;
     public float volume=0.5f;
     public float minY = 350f;
     public float maxY = 910f;
@@ -26,7 +27,7 @@ public class PlayerController : MonoBehaviour
     public float maxX = 1600f;
 
 
-    public bool expasiveShot = false;
+    public bool expansiveShot = false;
     public bool chaserShot = false;
     public bool x2Pts = false;
     public bool shield = false;
@@ -88,36 +89,84 @@ public class PlayerController : MonoBehaviour
             Instantiate(normalShot, attack_Point.position, Quaternion.identity);
         }
         else if (Input.GetKeyDown(KeyCode.V))
-        {
-            Instantiate(explosiveShot, attack_Point.position, Quaternion.identity); 
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            ActivateChaser();
+        { 
+            usePower();
         }
     }
 
     public void ActivateChaser(){
+        source.PlayOneShot(bonusSound, volume);
         chaserShot = true;
         ChaserItem chaserScript = GameObject.Find("ChaserShot").GetComponent<ChaserItem>();
         chaserScript.Activate();
     }
 
     public void ActivateExpansive(){
-        expasiveShot = true;
+        source.PlayOneShot(bonusSound, volume);
+        expansiveShot = true;
         ExpansiveItem expansiveScript = GameObject.Find("ExpansiveShot").GetComponent<ExpansiveItem>();
         expansiveScript.Activate();
     }
 
     public void ActivateShield(){
+        source.PlayOneShot(bonusSound, volume);
         shield = true;
         ShieldItem shieldScript = GameObject.Find("Shield").GetComponent<ShieldItem>();
         shieldScript.Activate();
     }
 
     public void ActivateX2Pts(){
+        source.PlayOneShot(bonusSound, volume);
         x2Pts = true;
         x2PtItem x2PtsScript = GameObject.Find("x2ptShot").GetComponent<x2PtItem>();
         x2PtsScript.Activate();
+    }
+
+    void usePower(){
+
+        PowerSelector powerScript = GameObject.Find("PowerSelector").GetComponent<PowerSelector>();
+        switch (powerScript.powerSelected)
+        {
+            case 0:
+                if (chaserShot)
+                {
+                    chaserShot = false;
+                    ChaserItem chaserScript = GameObject.Find("ChaserShot").GetComponent<ChaserItem>();
+                    chaserScript.Desactivate();
+                    Instantiate(explosiveShot, attack_Point.position, Quaternion.identity);
+                    Debug.Log("Chaser used and deactivated");
+                }
+                break;
+            case 1:
+                if (expansiveShot)
+                {
+                    expansiveShot = false;
+                    ExpansiveItem expansiveScript = GameObject.Find("ExpansiveShot").GetComponent<ExpansiveItem>();
+                    expansiveScript.Desactivate();
+                    Instantiate(explosiveShot, attack_Point.position, Quaternion.identity);
+                    Debug.Log("Expansive Shot used and deactivated");
+                }
+                break;
+            case 2:
+                if (x2Pts)
+                {
+                    x2Pts = false;
+                    x2PtItem x2PtsScript = GameObject.Find("x2ptShot").GetComponent<x2PtItem>();
+                    x2PtsScript.Desactivate();
+                    Instantiate(explosiveShot, attack_Point.position, Quaternion.identity);
+                    Debug.Log("x2Pts Shot used and deactivated");
+                }
+                break;
+            case 3:
+                if (shield)
+                {
+                    shield = false;
+                    ShieldItem shieldScript = GameObject.Find("Shield").GetComponent<ShieldItem>();
+                    shieldScript.Desactivate();
+                    Instantiate(explosiveShot, attack_Point.position, Quaternion.identity);
+                    Debug.Log("Shield Shot used and deactivated");
+                }
+                break;
+        }
     }
 }
